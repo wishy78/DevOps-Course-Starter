92c6951cf615c8ff1d58a38d20e0c0ac0a14a18e
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2022-03-22 11:46:49 +0000

    Tided up redy for submit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,10 @@
-
 from pydoc import render_doc
 from flask import Flask, redirect, render_template, request
-
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item
+
 app = Flask(__name__)
 app.config.from_object(Config())
-
 
 @app.route('/')
 def index():
