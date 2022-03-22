430673b205366e360d811d90f560c5c718db2d55
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2022-03-22 11:28:20 +0000

    To do app with list and initial form

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from pydoc import render_doc
+from flask import Flask, render_template
 
 from todo_app.flask_config import Config
-
+from todo_app.data.session_items import get_items
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html',items=items)
+
+@app.route('/new')
+def index2():
+    items = get_items()
+    return render_template('index.html',items=items)
