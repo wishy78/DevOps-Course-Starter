6fb43420943209f0c54e0962d654658b4e3f674a
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2022-04-21 16:24:26 +0100

    Task Class beginings off

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, redirect, render_template, request
 from todo_app.data.trello_items import add_card, get_cards, get_lists, move_card
 from todo_app.flask_config import Config
+from  todo_app.data.task_class import task,from_trello_card
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 @app.route('/')
 def index():
+    tt = task()
+    # so a class to hold a record
+    # so do i need multiple class items in a dictionary?
+    # where using a class to default settings so other systems can be used with minimal change
+    # eg task(ID, Name, State) (but this is in trello ID Name IDlist(name))
     cards = get_cards()
     lists = get_lists()
     return render_template('index.html',cards=cards,lists=lists)
