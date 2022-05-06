7377e2fd4853a5fb7a27f049a4d10f3508ad1dda
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2022-05-06 14:49:43 +0100

    a few tweaks based on code review

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,11 +1,10 @@
 
-from requests import get,post,put
-from flask import session
+import requests
 from os import getenv
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from todo_app.data.task_class import Task
 
-load_dotenv()
+#load_dotenv()
 
 KEY = getenv('TRELLO_KEY')
 TOKEN = getenv('TRELLO_TOKEN')
@@ -13,22 +12,22 @@ board = getenv('TRELLO_BOARD_ID')
 
 def get_lists():
     url = f"https://api.trello.com/1/boards/{board}/lists?fields=id,name,idBoard&key={KEY}&token={TOKEN}"
-    return get(url).json() 
+    return requests.get(url).json() 
 
 def get_cards():
     items = {}
     tasks = Task
     url = f"https://api.trello.com/1/boards/{board}/cards?fields=idList,name&key={KEY}&token={TOKEN}"
-    for card in get(url).json(): 
+    for card in requests.get(url).json(): 
         for list in get_lists():
             if card["idList"] == list["id"]:
                 card["listName"] = list["name"]
                 items[tasks.from_trello_card(card)] = tasks
-    return session.get('cards', items)
+    return items
 
 def move_card(card,list):
     url = f"https://api.trello.com/1/cards/{card}?key={KEY}&token={TOKEN}&idList={list}"
-    return put(url)
+    return requests.put(url)
 
 def add_card(title):
     listId = " "
@@ -37,7 +36,7 @@ def add_card(title):
             listId = list["id"]
             break
     url = f"https://api.trello.com/1/cards?idList={listId}&key={KEY}&token={TOKEN}" 
-    urldata = {'name': title, 'pos': 'top'}
-    return post(url,data = urldata)
+    urlbody = {'name': title, 'pos': 'top'}
+    return requests.post(url,data = urlbody)
 
 
