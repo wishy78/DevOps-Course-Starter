e7f26cb89161ef66531f12afd207360ac36675e5
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-02-01 11:36:49 +0000

    added user class and used it

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,10 +2,10 @@ from flask import Flask, redirect, render_template, request
 from todo_app.data.trello_items import add_card, get_cards, get_lists, move_card, read_env_deatils
 from todo_app.flask_config import Config
 from todo_app.View_Class import ViewModel
-from flask_login import LoginManager, login_required
+from flask_login import LoginManager, login_required, login_user
 from os import getenv
 import requests
-from requests import Response
+from todo_app.data.user_class import User
 
 def create_app():
     read_env_deatils()
@@ -21,11 +21,12 @@ def create_app():
     def unauthenticated():
         url = f"https://github.com/login/oauth/authorize?client_id={CLIENTID}&state={STATE}&redirect_uri={BASEURL}/login/callback&allow_signup=true"
         return redirect(url)
-    unauthenticated
     
     @login_manager.user_loader
     def load_user(user_id):
         pass # We will return to this later
+        thisuser = User(user_id)
+        return (thisuser)
     login_manager.init_app(app)
 
     @app.route('/')
@@ -50,27 +51,25 @@ def create_app():
     #@login_required
     def callback():
         args = request.args
-        request_token = args.get('code')
-        url = f'https://github.com/login/oauth/access_token?client_id={CLIENTID}\&client_secret={CLIENTSECRET}&code={request_token}'
-        headers = {'accept': 'application/json'}
-        res = requests.post(url, headers=headers)
-        data = res.json()
-        print('mmmmmmmmmmmmm')
-        print(args)
-        print(request_token)
-        print(url)
-        print(data)
-        
-        print('mmmmmmmmmmmmm')
+        recivedCode = args.get('code')
+        url = f'https://github.com/login/oauth/access_token?client_id={CLIENTID}&redirect_uri={BASEURL}/login/callback&client_secret={CLIENTSECRET}&code={recivedCode}'
+        headers = {'Accept': 'application/json'}
+        resp = requests.post(url=url, headers=headers)
+        data = resp.json()
         access_token = data['access_token']
-        access_token = 'token ' + access_token
-        url = 'https://api.github.com/user'
-        headers = {"Authorization": access_token}
-        resp = requests.get(url=url, headers=headers)
-        user_data = resp.json()
-        #return render_template('success.html', userData=user_data)
-        
-        return redirect('/',userData=user_data)
+        tempAT = 'Bearer '+access_token
+        url2 = 'https://api.github.com/user'
+        headers2 = {"Authorization": tempAT}
+        resp2 = requests.get(url=url2, headers=headers2)
+        user_data = resp2.json()
+        thisuser = User(user_data['id']) 
+        login_user(thisuser, remember=False, duration=None, force=True, fresh=True)
+        who = load_user(thisuser)
+        print(who)
+        return redirect('/')
 
+# upto step 3.5 ##########################################################
+#{'login': 'wishy78', 'id': 65459782, 'node_id': 'MDQ6VXNlcjY1NDU5Nzgy', 'avatar_url': 'https://avatars.githubusercontent.com/u/65459782?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/wishy78', 
+#'html_url': 'https://github.com/wishy78', 'followers_url': 'https://api.github.com/users/wishy78/followers', 'following_url': 'https://api.github.com/users/wishy78/following{/other_user}', 'gists_url': 'https://api.github.com/users/wishy78/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/wishy78/starred{/owner}{/repo}', 'subscriptions_url': 'https://api.github.com/users/wishy78/subscriptions', 'organizations_url': 'https://api.github.com/users/wishy78/orgs', 'repos_url': 'https://api.github.com/users/wishy78/repos', 'events_url': 'https://api.github.com/users/wishy78/events{/privacy}', 'received_events_url': 'https://api.github.com/users/wishy78/received_events', 'type': 'User', 'site_admin': False, 'name': None, 'company': None, 'blog': '', 'location': None, 'email': None, 'hireable': None, 'bio': None, 'twitter_username': None, 'public_repos': 9, 'public_gists': 0, 'followers': 0, 'following': 0, 'created_at': '2020-05-16T16:23:41Z', 'updated_at': '2022-09-29T09:36:17Z'}
     return app
 
