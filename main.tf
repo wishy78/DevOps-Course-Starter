830cbe0ed329d5299ce32dfe4947f1cc1a54dcde
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-02-23 12:36:24 +0000

    Added App to terraform

--- main.tf
+++ main.tf
@@ -14,3 +14,28 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
     name = "Cohort22_JonLon_ProjectExercise"
 }
+
+resource "azurerm_service_plan" "main" {
+  name = "terraformed-asp"
+  location = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name = "APP-JonL-Ex12"
+  location = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id = azurerm_service_plan.main.id
+  
+  site_config {
+    application_stack {
+      docker_image = "appsvcsample/python-helloworld"
+      docker_image_tag = "latest"
+    }
+  }
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+  }
+}
