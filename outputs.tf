85342300ad7827d2a8fcb992da9a171ccabcef47
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-02-27 13:59:13 +0000

    Remove-item alias:curl (to get curl working)

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "webapp_url" {
     value = "https://${azurerm_linux_web_app.main.default_hostname}"
 }
 
-output "weba_hook" {
+output "web_hook" {
     value = "https://${azurerm_linux_web_app.main.site_credential[0].name}:${azurerm_linux_web_app.main.site_credential[0].password}@${azurerm_linux_web_app.main.name}.scm.azurewebsites.net/docker/hook"
-    sensitive = true
+    sensitive = false
 }
