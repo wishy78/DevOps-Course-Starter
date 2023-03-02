fdd2e2b8976ba9a1133b6bb1f37bfb8b6968036d
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-03-02 12:40:18 +0000

    env

--- main.tf
+++ main.tf
@@ -17,20 +17,20 @@ terraform {
 provider "azurerm" {
     features {}
     
-    #subscription_id   = "${env.SUBSCRIPTION_ID}"
-    #tenant_id         = "${env.TENANT_ID}"
-    #client_id         = "${env.CLIENT_ID}"
-    #client_secret     = "${env.SUBSCRIPTION_ID}"
+    subscription_id   = "${env.SUBSCRIPTION_ID}"
+    tenant_id         = "${env.TENANT_ID}"
+    client_id         = "${env.CLIENT_ID}"
+    client_secret     = "${env.SUBSCRIPTION_ID}"
     
     #subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
     #tenant_id         = "${env.ARM_TENANT_ID}"
     #client_id         = "${env.ARM_CLIENT_ID}"
     #client_secret     = "${env.ARM_SUBSCRIPTION_ID}"
     
-    subscription_id   = "${var.SUBSCRIPTION_ID}"
-    tenant_id         = "${var.TENANT_ID}"
-    client_id         = "${var.CLIENT_ID}"
-    client_secret     = "${var.SUBSCRIPTION_ID}"
+    #subscription_id   = "${var.SUBSCRIPTION_ID}"
+    #tenant_id         = "${var.TENANT_ID}"
+    #client_id         = "${var.CLIENT_ID}"
+    #client_secret     = "${var.SUBSCRIPTION_ID}"
     
     #subscription_id   = "${secret.ARM_SUBSCRIPTION_ID}"
     #tenant_id         = "${secret.ARM_TENANT_ID}"
