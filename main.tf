3bac50e9ca8749777faf89374fe9fea191ec1302
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-03-02 12:35:03 +0000

    vars

--- main.tf
+++ main.tf
@@ -27,15 +27,15 @@ provider "azurerm" {
     #client_id         = "${env.ARM_CLIENT_ID}"
     #client_secret     = "${env.ARM_SUBSCRIPTION_ID}"
     
-    #subscription_id   = "${var.SUBSCRIPTION_ID}"
-    #tenant_id         = "${var.TENANT_ID}"
-    #client_id         = "${var.CLIENT_ID}"
-    #client_secret     = "${var.SUBSCRIPTION_ID}"
+    subscription_id   = "${var.SUBSCRIPTION_ID}"
+    tenant_id         = "${var.TENANT_ID}"
+    client_id         = "${var.CLIENT_ID}"
+    client_secret     = "${var.SUBSCRIPTION_ID}"
     
-    subscription_id   = "${secret.ARM_SUBSCRIPTION_ID}"
-    tenant_id         = "${secret.ARM_TENANT_ID}"
-    client_id         = "${secret.ARM_CLIENT_ID}"
-    client_secret     = "${secret.ARM_SUBSCRIPTION_ID}"
+    #subscription_id   = "${secret.ARM_SUBSCRIPTION_ID}"
+    #tenant_id         = "${secret.ARM_TENANT_ID}"
+    #client_id         = "${secret.ARM_CLIENT_ID}"
+    #client_secret     = "${secret.ARM_SUBSCRIPTION_ID}"
 }
 
 data "azurerm_resource_group" "main" {
