484bbad24999484f448ecc0d0008fb23d6e5f602
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-03-02 10:28:58 +0000

    env

--- main.tf
+++ main.tf
@@ -17,10 +17,14 @@ terraform {
 provider "azurerm" {
     features {}
     
-    subscription_id   = "${env.SUBSCRIPTION_ID}"
-    tenant_id         = "${env.TENANT_ID}"
-    client_id         = "${env.CLIENT_ID}"
-    client_secret     = "${env.SUBSCRIPTION_ID}"
+    #subscription_id   = "${env.SUBSCRIPTION_ID}"
+    #tenant_id         = "${env.TENANT_ID}"
+    #client_id         = "${env.CLIENT_ID}"
+    #client_secret     = "${env.SUBSCRIPTION_ID}"
+    subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
+    tenant_id         = "${env.ARM_TENANT_ID}"
+    client_id         = "${env.ARM_CLIENT_ID}"
+    client_secret     = "${env.ARM_SUBSCRIPTION_ID}"
     #subscription_id   = "${var.SUBSCRIPTION_ID}"
     #tenant_id         = "${var.TENANT_ID}"
     #client_id         = "${var.CLIENT_ID}"
