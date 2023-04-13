68cae8187a77603937dcd28ba57a297cd70e9767
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-04-13 16:21:02 +0100

    now provider skip in right place?

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
     required_providers {
+        skip_provider_registration = "true"
         azurerm = {
-          skip_provider_registration = "true"
           source = "hashicorp/azurerm"
           version = ">= 3.8"
         }
